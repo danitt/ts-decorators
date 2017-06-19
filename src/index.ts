b4056d0ba6b9fb875bf35e0fdd7dd0906b7bae70
dan <dan@danitt.com>
2017-06-19 03:16:30 +0200

    💥 test basic class decorator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
 console.log('hello world')
+
+class Addy {
+  @log
+  add(a, b) {
+    return a + b
+  }
+}
+
+function log(targetClass, functionName, targetDescriptor) {
+  // Get Old Data
+  const oldValue = targetDescriptor.value
+  // Modify Data
+  targetDescriptor.value = function() {
+    // Can only get real-time arguments here
+    const classArguments = arguments
+    console.log(`Calling "${functionName}" with`, classArguments)
+    return oldValue.apply(null, classArguments)
+  }
+  // Return Descriptor
+  return targetDescriptor
+}
+
+new Addy().add(2, 4)
